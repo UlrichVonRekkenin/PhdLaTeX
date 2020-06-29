a83a2a1796051e6ed69d4373941bfdac88e94a00
ulrich <ulrich@foobar.com>
2020-06-29 14:31:52 +0300

    Улучшения и немного текста
    -уточнил описание periodic
    -namespace modbus::io
    -новая схема в замен конструктора
    -цветовое объединение в диаграмме полследовательностей.

---
+++ Dissertation/listings/cpp/comparator.hpp
@@ -0,0 +1,17 @@
+class Comparator final {
+public:
+[[nodiscard]] static bool 
+compare(const modbusio::ModbusDataRelationed& cond, 
+        const modbusio::ModbusData& data)
+{
+    switch (cond.relation()) {
+        case Relation::Eq: return data.value()==cond.value();
+        case Relation::Ne: return data.value()!=cond.value();
+        case Relation::Gt: return data.value()>cond.value();
+        case Relation::Lt: return data.value()<cond.value();
+        case Relation::Ge: return data.value()>=cond.value();
+        case Relation::Le: return data.value()<=cond.value();
+    }
+    return false;
+}
+};
