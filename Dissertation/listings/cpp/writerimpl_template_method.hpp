1ff4002162cfb31c2b50ee1c85b638fbddb25b5b
ulrich <ulrich@foobar.com>
2020-06-30 17:20:09 +0300

    Описание работы менеджера

--- Dissertation/listings/cpp/writerimpl_template_method.hpp
+++ Dissertation/listings/cpp/writerimpl_template_method.hpp
@@ -1,14 +1,14 @@
-class IModbusElementWriterImpl : public IModbusElementWriter {
+class ModbusElementWriterImpl : public QObject, public IModbusElementWriter {
 // ...
 public slots:
-void IModbusElementWriterImpl::newModbusData(const modbus::ModbusData& data) override {
+void ModbusElementWriterImpl::newModbusData(const modbus::ModbusData& data) override {
     if (_running && conditionsMet(data)) {
         write();
     }
 }
 
 protected:
-void IModbusElementWriterImpl::write() override {
+void ModbusElementWriterImpl::write() override {
     if (_deviceWriter.get()) {
         _deviceWriter->write(static_cast<const modbus::ModbusData>(*this));
         emit writed(static_cast<const modbus::ModbusData>(*this));
